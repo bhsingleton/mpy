fd77d3b009641af75e4ba7fd0c166c285ef525d6
bhsingleton <bhsingleton@gmail.com>
2022-10-27 16:13:56 -0400

    Adding shake node interface.

---
+++ plugintypes/shakemixin.py
@@ -0,0 +1,36 @@
+from maya.api import OpenMaya as om
+from .. import mpyattribute
+from ..nodetypes import dependencymixin
+
+import logging
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+
+class ShakeMixin(dependencymixin.DependencyMixin):
+    """
+    Overload of DependencyMixin that interfaces with shake nodes.
+    """
+
+    # region Dunderscores
+    __plugin__ = 'shake'
+    # endregion
+
+    # region Attributes
+    envelope = mpyattribute.MPyAttribute('envelope')
+    seed = mpyattribute.MPyAttribute('seed')
+    frequency = mpyattribute.MPyAttribute('frequency')
+    fractal = mpyattribute.MPyAttribute('fractal')
+    roughness = mpyattribute.MPyAttribute('roughness')
+    rampIn = mpyattribute.MPyAttribute('rampIn')
+    rampOut = mpyattribute.MPyAttribute('rampOut')
+    strength = mpyattribute.MPyAttribute('strength')
+    strengthX = mpyattribute.MPyAttribute('strengthX')
+    strengthY = mpyattribute.MPyAttribute('strengthY')
+    strengthZ = mpyattribute.MPyAttribute('strengthZ')
+    positive = mpyattribute.MPyAttribute('positive')
+    positiveX = mpyattribute.MPyAttribute('positiveX')
+    positiveY = mpyattribute.MPyAttribute('positiveY')
+    positiveZ = mpyattribute.MPyAttribute('positiveZ')
+    # endregion
