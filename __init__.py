72159fbe0c2f93083ebca011177da12bc2afa4ba
Ben Singleton <bhsingleton@gmail.com>
2023-02-17 17:15:46 -0500

    Improving restart logic.

--- __init__.py
+++ __init__.py
@@ -3,45 +3,40 @@ import sys
 
 import logging
 logging.basicConfig()
-log = logging.getLogger(__name__)
+log = logging.getLogger('mpy')
 log.setLevel(logging.INFO)
 
 
-def rollback():
+def rollback(*paths):
     """
     Removes all modules, derived from the mpy package, from the system module.
     This will force any future imported modules to recompile.
 
+    :type paths: Union[str, List[str]]
     :rtype: None
     """
 
     # Iterate through modules
-    # Use this package to differentiate modules
     #
-    directory = os.path.dirname(os.path.abspath(__file__))
     moduleNames = set(sys.modules.keys())
+    paths = [os.path.dirname(os.path.abspath(path)) if os.path.isfile(path) else os.path.abspath(path) for path in paths if os.path.exists(path)]
 
     for moduleName in moduleNames:
 
-        # Check if module has file attribute
+        # Get path to module
         #
         module = sys.modules[moduleName]
+        filePath = getattr(module, '__file__', None)
 
-        if not hasattr(module, '__file__'):
-
-            continue
-
-        # Check for none type
-        #
-        if module.__file__ is None:
+        if filePath is None:
 
             continue
 
-        # Check if module falls under python paths
+        # Check if module is derived from any of the supplied paths
         #
-        filePath = os.path.abspath(module.__file__)
+        filePath = os.path.abspath(os.path.normpath(filePath))
 
-        if filePath.startswith(directory):
+        if any([filePath.startswith(path) for path in paths]):
 
             log.info('Rolling back module: %s' % module)
             del sys.modules[moduleName]
@@ -55,4 +50,4 @@ def restart():
     :rtype: None
     """
 
-    rollback()
+    rollback(os.path.dirname(os.path.abspath(__file__)))
