d311167aae388b86784e42e3e2d18f1f4a6df55f
Ben Singleton <bhsingleton@gmail.com>
2024-07-02 17:34:45 -0400

    Overload `detectMirroring` method.

--- plugins/freeformmixin.py
+++ plugins/freeformmixin.py
@@ -1,3 +1,5 @@
+import math
+
 from maya.api import OpenMaya as om
 from dcc.maya.libs import transformutils
 from .. import mpyattribute
@@ -26,7 +28,7 @@ class FreeformMixin(transformmixin.TransformMixin):
         :rtype: om.MEulerRotation
         """
 
-        return om.MEulerRotation(self.getAttr('rotateAxis', convertUnits=False))
+        return om.MEulerRotation(list(map(math.radians, self.getAttr('rotateAxis'))))
 
     def setPreEulerRotation(self, eulerRotation):
         """
@@ -37,4 +39,58 @@ class FreeformMixin(transformmixin.TransformMixin):
         """
 
         self.setAttr('rotateAxis', eulerRotation, convertUnits=False)
+
+    def detectMirroring(self, normal=om.MVector.kXaxisVector):
+        """
+        Detects the mirror settings for this transform.
+        Unlike the parent method, this overload takes the pre-rotation into consideration.
+
+        :type normal: om.MVector
+        :rtype: bool
+        """
+
+        # Compare parent matrices for translate settings
+        #
+        matrix = self.parentMatrix()
+        xAxis, yAxis, zAxis, pos = transformutils.breakMatrix(matrix, normalize=True)
+        mirrorXAxis, mirrorYAxis, mirrorZAxis = [transformutils.mirrorVector(axis, normal=normal) for axis in (xAxis, yAxis, zAxis)]
+
+        otherTransform = self.getOppositeNode()
+        otherMatrix = otherTransform.parentMatrix()
+        otherXAxis, otherYAxis, otherZAxis, otherPos = transformutils.breakMatrix(otherMatrix, normalize=True)
+
+        mirrorTranslateX = (mirrorXAxis * otherXAxis) < 0.0
+        mirrorTranslateY = (mirrorYAxis * otherYAxis) < 0.0
+        mirrorTranslateZ = (mirrorZAxis * otherZAxis) < 0.0
+
+        # Compare parent matrices, again, but this time with pre-rotations for rotate settings
+        #
+        preEulerRotation = self.preEulerRotation()
+        matrix = preEulerRotation.asMatrix() * self.parentMatrix()
+        xAxis, yAxis, zAxis, pos = transformutils.breakMatrix(matrix, normalize=True)
+        mirrorXAxis, mirrorYAxis, mirrorZAxis = [transformutils.mirrorVector(axis, normal=normal) for axis in (xAxis, yAxis, zAxis)]
+
+        otherPreEulerRotation = otherTransform.preEulerRotation()
+        otherMatrix = otherPreEulerRotation.asMatrix() * otherTransform.parentMatrix()
+        otherXAxis, otherYAxis, otherZAxis, otherPos = transformutils.breakMatrix(otherMatrix, normalize=True)
+
+        mirrorRotateX = (mirrorXAxis * otherXAxis) > 0.0
+        mirrorRotateY = (mirrorYAxis * otherYAxis) > 0.0
+        mirrorRotateZ = (mirrorZAxis * otherZAxis) > 0.0
+
+        # Compose mirror settings and update user properties
+        #
+        settings = {
+            'mirrorTranslateX': mirrorTranslateX,
+            'mirrorTranslateY': mirrorTranslateY,
+            'mirrorTranslateZ': mirrorTranslateZ,
+            'mirrorRotateX': mirrorRotateX,
+            'mirrorRotateY': mirrorRotateY,
+            'mirrorRotateZ': mirrorRotateZ,
+        }
+
+        log.info('Detecting "%s" > "%s" mirror settings: %s' % (self.name(), otherTransform.name(), settings))
+        self.userProperties.update(settings)
+
+        return True
     # endregion
