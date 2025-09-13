e14e9aabcfb652fa39e91c9f5b384f386c011f8b
Ben Singleton <bhsingleton@gmail.com>
2025-09-13 10:08:06 -0400

    Propagating changes made to remap array plugin.

--- plugins/remaparraymixin.py
+++ plugins/remaparraymixin.py
@@ -1,4 +1,5 @@
 from maya.api import OpenMaya as om
+from enum import IntEnum
 from .. import mpyattribute
 from ..builtins import dependencymixin
 
@@ -8,17 +9,34 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 
+class Function(IntEnum):
+    """
+    Enum class of all available remap functions.
+    """
+
+    CUSTOM = 0
+    EASE_IN = 1
+    EASE_OUT = 2
+    EASE_BOTH = 3
+
+
 class RemapArrayMixin(dependencymixin.DependencyMixin):
     """
     Overload of `DependencyMixin` that interfaces with remap-array nodes.
     """
+
+    # region Enums
+    Function = Function
+    # endregion
 
     # region Dunderscores
     __plugin__ = 'remapArray'
     # endregion
 
     # region Attributes
-    inValue = mpyattribute.MPyAttribute('inValue')
+    function = mpyattribute.MPyAttribute('function')
+    coefficient = mpyattribute.MPyAttribute('coefficient')
+    parameter = mpyattribute.MPyAttribute('parameter')
     value = mpyattribute.MPyAttribute('value')
     color = mpyattribute.MPyAttribute('color')
     clamp = mpyattribute.MPyAttribute('clamp')
